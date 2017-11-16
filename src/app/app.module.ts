6020b27121e77b29b25895e9da4e17f55885e74c
SibeeshVenu <sibikv4u@gmail.com>
2017-11-16 21:38:15 +0530

    Generating Your First Components And Modules in Angular 5 App, Angular Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { RegistrationComponent } from './registration/registration.component';
 
+const myRoots: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegistrationComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule, MatToolbarModule,
+    RouterModule.forRoot(myRoots)
   ],
   providers: [],
   bootstrap: [AppComponent]
