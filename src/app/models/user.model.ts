a1fed67a3495281e28226939ca0bf393ca1d4059
SibeeshVenu <sibikv4u@gmail.com>
2018-04-22 15:06:08 +0530

    Validation Using Template Driven Forms in Angular 5

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+    id: any;
+    userName: string;
+    email: string;
+    userRole: string;
+    profileImage: string;
+    phoneNumber: string;
+    firstName: string;
+    lastName: string;
+}
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,37 +1,20 @@
-﻿import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-
+﻿import { OnInit, Component } from "@angular/core";
+import { User } from '../models/user.model';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-
-  form;
-  constructor(private fb: FormBuilder) {
-    this.form = fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', [Validators.required, isEmailValid('email')]],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required]
-    });
-  }
-
-  register() {
-    console.log(this.form.value);
+  user = new User();
+  constructor() {
   }
 
   ngOnInit() {
   }
 
-}
-
-function isEmailValid(control) {
-  return control => {
-    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return regex.test(control.value) ? null : { invalidEmail: true };
+  register(user: User): void{
+    console.log(user);
   }
 }
 
